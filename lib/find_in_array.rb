b7faae81b3222a767b989a4c5d79a24686ac14ba
Danira Cortez <cortezd334@gmail.com>
2020-05-11 23:02:59 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
-  if array.times.include?(value_to_find)
-    array.times.include?(value_to_find)
+  if array.length.include?(value_to_find)
+    array.length.include?(value_to_find)
   else nil
   end
 end
