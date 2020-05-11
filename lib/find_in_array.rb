1ba3c1cb87bbe79312a24dcf00fdb85ef21d18b8
Danira Cortez <cortezd334@gmail.com>
2020-05-11 23:17:51 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
 count = 0
-  while count < array.length
-      if array.include?(value_to_find)
-        return array[count]
-      else
-        return nil
+  while count < array.length do
+    if array.include?(value_to_find)
+      return array[count]
+    else
+      return nil
     count += 1
   end
   end
