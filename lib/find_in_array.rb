58715fc6b29a3780f1ba316307938607153abcaf
Danira Cortez <cortezd334@gmail.com>
2020-05-11 23:04:22 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,5 @@
 def find_element_index(array, value_to_find)
-  if array.length.include?(value_to_find)
+  if array.include?(value_to_find)
     array.length.include?(value_to_find)
   else nil
   end
