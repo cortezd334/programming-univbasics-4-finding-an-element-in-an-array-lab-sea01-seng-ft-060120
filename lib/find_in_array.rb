0bb201218b2b716ef8ec6d0a04b226d1f3b63fc3
Danira Cortez <cortezd334@gmail.com>
2020-05-11 23:26:13 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
 count = 0
   while count < array.length do
     if array.include?(value_to_find)
-      return count
+      puts count
     end
     count += 1
   end
