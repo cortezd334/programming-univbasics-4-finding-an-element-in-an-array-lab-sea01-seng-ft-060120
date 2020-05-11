e8a51a5a305e0fb00e3f38b4003c200df2300220
Danira Cortez <cortezd334@gmail.com>
2020-05-11 23:10:35 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
 count = 0
   while count < array.length
       if array.include?(value_to_find)
-        return array[count]
+        return array[count].include?(value_to_find)
       else 
         return nil
     count += 1
